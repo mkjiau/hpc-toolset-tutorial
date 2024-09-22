242b1a4f27a6559eddf5a518b7944a1396e2729f
Ming-Kai Jiau <kasimjm7997@gmail.com>
2024-09-23 01:08:00 +0800

    fix: HPCTS_VERSION, MARIADB_VERSION, numpy==1.26

--- slurm/install.sh
+++ slurm/install.sh
@@ -50,6 +50,7 @@ wget -O /tmp/websockify-${WEBSOCKIFY_VERSION}.tar.gz https://github.com/novnc/we
 pushd /tmp
 tar xzf websockify-${WEBSOCKIFY_VERSION}.tar.gz
 pushd websockify-${WEBSOCKIFY_VERSION}
+sed -i 's/numpy/numpy==1.26/' setup.py
 python3 setup.py install
 popd
 rm -rf /tmp/websockify*
